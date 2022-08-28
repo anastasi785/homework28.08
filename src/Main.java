f9472a57271fd1596323a4d10b6e24ea334d2c13
nastu <nastushik92@bk.ru>
2022-08-28 18:55:24 +0300

    комит по дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        int[] weights ={1,2,3};
+        System.out.println(weights[0]);
+        System.out.println(weights[1]);
+        System.out.println(weights[2]);
+        double[] weight1 = {1.57, 7.654, 9.986};
+        System.out.println(weight1[0]);
+        System.out.println(weight1[1]);
+        System.out.println(weight1[2]);
+
+
+    // задание 2
+        System.out.println(weights[0]+","+weights[1]+","+weights[2]+ " "+weight1[0]+","+weight1[1]+","+weight1[2]);
+        //задание 3
+
+        System.out.println(weights[2]+","+weights[1]+","+weights[0]+ " "+weight1[2]+","+weight1[1]+","+weight1[0]);
+
+        //задание 4
+        int i = weights[0];
+        for(i=1; i<2;i=+1);
+        System.out.println (i+1);
+
+        }
+
     }
-}
+
